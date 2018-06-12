a221e6212e8e8e89a4c8d743814db437e0e8fc0f
Darren-Ivey <divey@mylifedigital.co.uk>
2018-06-12 15:31:18 +0100

    Update test

--- src/module/rockPaperScissors-mocha.test.js
+++ src/module/rockPaperScissors-mocha.test.js
@@ -1,15 +1,15 @@
 const itParam = require('mocha-param');
-const expect = require('npm i chai').expect;
-import { rockPaperScissors } from './rockPaperScissors';
+const expect = require('chai').expect;
+const rockPaperScissors = require('./rockPaperScissors').rockPaperScissors;
 
 describe('Rock blunts Scissors:', () => {
 
     it('player one should win if he plays Rock against Scissors', () => {
-        expect(new rockPaperScissors.game('rock', 'scissors')).to.equal('player1');
-    })
+        expect(rockPaperScissors.game('rock', 'scissors')).to.equal('player1');
+    });
 
     it('player two should win if he plays Rock against Scissors', () => {
-        expect(new rockPaperScissors.game('scissors', 'rock')).to.equal('player2');
+        expect(rockPaperScissors.game('scissors', 'rock')).to.equal('player2');
     })
 
 });
@@ -17,11 +17,11 @@ describe('Rock blunts Scissors:', () => {
 describe('Scissors cuts Paper:', () => {
 
     it('player one should win if he plays Scissors against Paper', () => {
-        expect(new rockPaperScissors.game('scissors', 'paper')).to.equal('player1');
-    })
+        expect(rockPaperScissors.game('scissors', 'paper')).to.equal('player1');
+    });
 
     it('player two should win if he plays rock against scissors', () => {
-        expect(new rockPaperScissors.game('paper', 'scissors')).to.equal('player2');
+        expect(rockPaperScissors.game('paper', 'scissors')).to.equal('player2');
     })
 
 });
@@ -29,11 +29,11 @@ describe('Scissors cuts Paper:', () => {
 describe('Paper wraps Stone:', () => {
 
     it('player one should win if he plays Paper against Rock', () => {
-        expect(new rockPaperScissors.game('paper', 'rock')).to.equal('player1');
-    })
+        expect(rockPaperScissors.game('paper', 'rock')).to.equal('player1');
+    });
 
     it('player two should win if he plays Paper against Rock', () => {
-        expect(new rockPaperScissors.game('rock', 'paper')).to.equal('player2');
+        expect(rockPaperScissors.game('rock', 'paper')).to.equal('player2');
     })
 
 });
@@ -43,6 +43,6 @@ describe('It should be a draw if both players play the same thing', () => {
     const games = [{ playerOne: 'rock', playerTwo: 'rock' }, { playerOne: 'paper', playerTwo: 'paper' }, { playerOne: 'scissors', playerTwo: 'scissors' }];
 
     itParam("if player one plays ${value.playerOne} and player two plays ${value.playerTwo} it's a draw", games, ({playerOne, playerTwo}) => {
-        expect(new rockPaperScissors.game(playerOne, playerTwo)).to.equal('draw')
+        expect(rockPaperScissors.game(playerOne, playerTwo)).to.equal('draw')
     })
 });
