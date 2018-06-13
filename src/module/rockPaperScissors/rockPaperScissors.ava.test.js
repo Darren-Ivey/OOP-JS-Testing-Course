6716fcbe44fc5c2e81c660192bd863dfe60b322b
Darren-Ivey <divey@mylifedigital.co.uk>
2018-06-13 14:54:03 +0100

    Fix breaking test

--- src/module/rockPaperScissors/rockPaperScissors.ava.test.js
+++ src/module/rockPaperScissors/rockPaperScissors.ava.test.js
@@ -3,22 +3,22 @@ import test from 'ava'
 import { rockPaperScissors } from './rockPaperScissors';
 
 test('rock beat scissors', t => {
-    t.is(rockPaperScissors.game('rock', 'scissors') , "player1");
-    t.is(rockPaperScissors.game('scissors', 'rock') , "player2");
+    t.is(rockPaperScissors.round('rock', 'scissors') , "player1");
+    t.is(rockPaperScissors.round('scissors', 'rock') , "player2");
 });
 
 test('paper beat rock', t => {
-    t.is(rockPaperScissors.game('paper', 'rock') , "player1");
-    t.is(rockPaperScissors.game('rock', 'paper') , "player2");
+    t.is(rockPaperScissors.round('paper', 'rock') , "player1");
+    t.is(rockPaperScissors.round('rock', 'paper') , "player2");
 });
 
 test('scissors beat paper', t => {
-    t.is(rockPaperScissors.game('scissors', 'paper') , "player1");
-    t.is(rockPaperScissors.game('paper', 'scissors') , "player2");
+    t.is(rockPaperScissors.round('scissors', 'paper') , "player1");
+    t.is(rockPaperScissors.round('paper', 'scissors') , "player2");
 });
 
 test('draw', t => {
-    t.is(rockPaperScissors.game('rock', 'rock') , "draw");
-    t.is(rockPaperScissors.game('scissors', 'scissors') , "draw");
-    t.is(rockPaperScissors.game('paper', 'paper') , "draw");
+    t.is(rockPaperScissors.round('rock', 'rock') , "draw");
+    t.is(rockPaperScissors.round('scissors', 'scissors') , "draw");
+    t.is(rockPaperScissors.round('paper', 'paper') , "draw");
 });
--- src/module/rockPaperScissors/rockPaperScissors.mocha.test.js
+++ src/module/rockPaperScissors/rockPaperScissors.mocha.test.js
@@ -5,11 +5,11 @@ const rockPaperScissors = require('./rockPaperScissors').rockPaperScissors;
 describe('Rock blunts Scissors:', () => {
 
     it('player one should win if he plays Rock against Scissors', () => {
-        expect(rockPaperScissors.game('rock', 'scissors')).to.equal('player1');
+        expect(rockPaperScissors.round('rock', 'scissors')).to.equal('player1');
     });
 
     it('player two should win if he plays Rock against Scissors', () => {
-        expect(rockPaperScissors.game('scissors', 'rock')).to.equal('player2');
+        expect(rockPaperScissors.round('scissors', 'rock')).to.equal('player2');
     })
 
 });
@@ -17,11 +17,11 @@ describe('Rock blunts Scissors:', () => {
 describe('Scissors cuts Paper:', () => {
 
     it('player one should win if he plays Scissors against Paper', () => {
-        expect(rockPaperScissors.game('scissors', 'paper')).to.equal('player1');
+        expect(rockPaperScissors.round('scissors', 'paper')).to.equal('player1');
     });
 
     it('player two should win if he plays rock against scissors', () => {
-        expect(rockPaperScissors.game('paper', 'scissors')).to.equal('player2');
+        expect(rockPaperScissors.round('paper', 'scissors')).to.equal('player2');
     })
 
 });
@@ -29,11 +29,11 @@ describe('Scissors cuts Paper:', () => {
 describe('Paper wraps Stone:', () => {
 
     it('player one should win if he plays Paper against Rock', () => {
-        expect(rockPaperScissors.game('paper', 'rock')).to.equal('player1');
+        expect(rockPaperScissors.round('paper', 'rock')).to.equal('player1');
     });
 
     it('player two should win if he plays Paper against Rock', () => {
-        expect(rockPaperScissors.game('rock', 'paper')).to.equal('player2');
+        expect(rockPaperScissors.round('rock', 'paper')).to.equal('player2');
     })
 
 });
@@ -43,6 +43,6 @@ describe('It should be a draw if both players play the same thing', () => {
     const games = [{ playerOne: 'rock', playerTwo: 'rock' }, { playerOne: 'paper', playerTwo: 'paper' }, { playerOne: 'scissors', playerTwo: 'scissors' }];
 
     itParam("if player one plays ${value.playerOne} and player two plays ${value.playerTwo} it's a draw", games, ({playerOne, playerTwo}) => {
-        expect(rockPaperScissors.game(playerOne, playerTwo)).to.equal('draw')
+        expect(rockPaperScissors.round(playerOne, playerTwo)).to.equal('draw')
     })
 });
